20cd2a018313c6b810dd6d0d6b918c301a0cddc4
mcelaney <brian.e.mcelaney@gmail.com>
2013-06-15 23:18:29 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateMugwarrior::Application.config.secret_token = '5a7e873d6b8efe8222ecd1e717b0fae7095edb681f14dbf231a8961f7865def2becf91d21e053687ff234f7845bafaf12f8bb07c4bd036683c1f135a561f5872'
