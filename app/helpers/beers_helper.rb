55d78c1e7d63d6ef2259e84c85b86220a63e805b
Brian McElaney <brian.e.mcelaney@gmail.com>
2013-07-17 22:50:07 -0400

    light formatting

--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -2,11 +2,13 @@ module BeersHelper
 
   def link_to_next_beer_ranking
     beer = Beer.find_random_unranked_beer_for(current_user)
+
     if beer
       link_to "Rank", get_beer_url(beer.id)
     else
       "All beers Ranked"
     end
+
   end
 
   def url_for_next_beer_ranking
@@ -17,6 +19,7 @@ module BeersHelper
     else
       return beers_path
     end
+
   end
 
 
